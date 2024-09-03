6f51a1a7b3faefbe529cee3da3ff82f6658c53f5
Mirza Alby Assidiqie <94514652+mirzajebret@users.noreply.github.com>
2024-09-03 09:11:25 +0700

    revision 2

---
+++ script.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Load header and footer
+  Promise.all([
+      fetch('header.html').then(response => response.text()),
+      fetch('footer.html').then(response => response.text())
+  ]).then(([headerData, footerData]) => {
+      document.getElementById('header').innerHTML = headerData;
+      document.getElementById('footer').innerHTML = footerData;
+  }).catch(error => console.error('Error loading header or footer:', error));
+
+  // Back to Top Button
+  const backToTopButton = document.getElementById('back-to-top');
+  
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 200) { // Show the button if scrolled down more than 200px
+          backToTopButton.style.display = 'block';
+      } else {
+          backToTopButton.style.display = 'none';
+      }
+
+      // Adjust header size based on scroll position
+      const header = document.getElementById('header');
+      if (window.scrollY > 150) { // Adjust the threshold as needed
+          header.classList.add('shrink');
+      } else {
+          header.classList.remove('shrink');
+      }
+  });
+
+  // Smooth scroll to the top when the button is clicked
+  backToTopButton.addEventListener('click', function() {
+      window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+      });
+  });
+});
+
+function searchServices() {
+    // Ambil nilai input dari pengguna
+    var input = document.getElementById('searchInput');
+    var filter = input.value.toLowerCase();
+    
+    // Ambil semua elemen service-box
+    var serviceBoxes = document.getElementsByClassName('service-box');
+    
+    // Loop melalui semua service-box
+    for (var i = 0; i < serviceBoxes.length; i++) {
+      var h3 = serviceBoxes[i].getElementsByTagName("h3")[0];
+      var ul = serviceBoxes[i].getElementsByTagName("ul")[0];
+      var serviceText = h3.textContent || h3.innerText;
+      var ulText = ul.textContent || ul.innerText;
+      
+      // Jika ada teks yang cocok dengan input pengguna, tampilkan elemen
+      if (serviceText.toLowerCase().indexOf(filter) > -1 || ulText.toLowerCase().indexOf(filter) > -1) {
+        serviceBoxes[i].style.display = "";
+      } else {
+        serviceBoxes[i].style.display = "none";
+      }
+    }
+  }
