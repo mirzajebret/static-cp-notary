e779f5acaf6e444821dd5f1ef05696df91e1139a
Mirza Alby Assidiqie <94514652+mirzajebret@users.noreply.github.com>
2024-09-09 16:15:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ function searchServices() {
     const dots = document.querySelectorAll('.dot');
     const items = document.querySelectorAll('.carousel-item');
     let index = 0;
-    const slideInterval = 3000; // Slide every 3 seconds
+    const slideInterval = 5000; // Slide every 5 seconds
 
     function updateCarousel() {
         const offset = -index * 100;
