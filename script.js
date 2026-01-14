8e7d1f8e92d7e344db4ba6296ab40e01d8dc89e8
mirzajebret <assidiqiemirza@gmail.com>
2026-01-14 16:05:24 +0700

    fix header footer

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function toggleDropdown(id) {
   var dropdown = document.getElementById(id);
   var allDropdowns = document.querySelectorAll('.dropdown-content');
-  
+
   // Hide all other dropdowns
-  allDropdowns.forEach(function(d) {
+  allDropdowns.forEach(function (d) {
     if (d !== dropdown) {
       d.classList.remove('show');
     }
@@ -16,7 +16,7 @@ function toggleDropdown(id) {
 // Function to handle clicks outside of dropdowns
 function handleClickOutside(event) {
   var allDropdowns = document.querySelectorAll('.dropdown-content');
-  allDropdowns.forEach(function(d) {
+  allDropdowns.forEach(function (d) {
     if (!d.contains(event.target) && !document.querySelector(`.item[data-dropdown="${d.id}"]`).contains(event.target)) {
       d.classList.remove('show');
     }
@@ -28,94 +28,94 @@ document.addEventListener('click', handleClickOutside);
 
 // Function to search services
 function searchServices() {
-    var input = document.getElementById('searchInput');
-    var filter = input.value.toLowerCase();
-    var items = document.querySelectorAll('.tabel-layanan .item');
-    
-    items.forEach(function(item) {
-        var text = item.textContent || item.innerText;
-        if (text.toLowerCase().indexOf(filter) > -1) {
-            item.style.display = "";
-        } else {
-            item.style.display = "none";
-        }
-    });
+  var input = document.getElementById('searchInput');
+  var filter = input.value.toLowerCase();
+  var items = document.querySelectorAll('.tabel-layanan .item');
+
+  items.forEach(function (item) {
+    var text = item.textContent || item.innerText;
+    if (text.toLowerCase().indexOf(filter) > -1) {
+      item.style.display = "";
+    } else {
+      item.style.display = "none";
+    }
+  });
 }
 
-  document.addEventListener('DOMContentLoaded', function() {
-      // Load header and footer
-      Promise.all([
-          fetch('header.html').then(response => response.text()),
-          fetch('footer.html').then(response => response.text())
-      ]).then(([headerData, footerData]) => {
-          document.getElementById('header').innerHTML = headerData;
-          document.getElementById('footer').innerHTML = footerData;
-  
-          // Initialize after header content is loaded
-          initializeNavigation();
-      }).catch(error => console.error('Error loading header or footer:', error));
-    
-      function initializeNavigation() {
-        const backToTopButton = document.getElementById('back-to-top');
-    
-        
-    
-        // Initialize menu button functionality
-        const menuButton = document.querySelector('.menu-button');
-        const navLinks = document.querySelector('.nav-links');
-        
-        if (menuButton && navLinks) {
-            menuButton.addEventListener('click', function() {
-                navLinks.classList.toggle('active');
-            });
-        }
-      }
+document.addEventListener('DOMContentLoaded', function () {
+  // Load header and footer
+  Promise.all([
+    fetch('../header.html').then(response => response.text()),
+    fetch('../footer.html').then(response => response.text())
+  ]).then(([headerData, footerData]) => {
+    document.getElementById('header').innerHTML = headerData;
+    document.getElementById('footer').innerHTML = footerData;
+
+    // Initialize after header content is loaded
+    initializeNavigation();
+  }).catch(error => console.error('Error loading header or footer:', error));
+
+  function initializeNavigation() {
+    const backToTopButton = document.getElementById('back-to-top');
 
 
-    //  carousel
-    const carouselInner = document.querySelector('.carousel-inner');
-    const dots = document.querySelectorAll('.dot');
-    const items = document.querySelectorAll('.carousel-item');
-    let index = 0;
-    const slideInterval = 1700; // Slide every 3 seconds
 
-    function updateCarousel() {
-        const offset = -index * 100;
-        carouselInner.style.transform = `translateX(${offset}%)`;
-        updateDots();
+    // Initialize menu button functionality
+    const menuButton = document.querySelector('.menu-button');
+    const navLinks = document.querySelector('.nav-links');
+
+    if (menuButton && navLinks) {
+      menuButton.addEventListener('click', function () {
+        navLinks.classList.toggle('active');
+      });
     }
+  }
 
-    function updateDots() {
-        dots.forEach((dot, i) => {
-            dot.classList.toggle('active', i === index);
-        });
-    }
 
-    function nextSlide() {
-        index = (index < items.length - 1) ? index + 1 : 0;
-        updateCarousel();
-    }
+  //  carousel
+  const carouselInner = document.querySelector('.carousel-inner');
+  const dots = document.querySelectorAll('.dot');
+  const items = document.querySelectorAll('.carousel-item');
+  let index = 0;
+  const slideInterval = 1700; // Slide every 3 seconds
 
-    function setSlide(newIndex) {
-        index = newIndex;
-        updateCarousel();
-    }
+  function updateCarousel() {
+    const offset = -index * 100;
+    carouselInner.style.transform = `translateX(${offset}%)`;
+    updateDots();
+  }
 
-    // Auto-slide every `slideInterval` milliseconds
-    setInterval(nextSlide, slideInterval);
-
-    // Set up dot click events
+  function updateDots() {
     dots.forEach((dot, i) => {
-        dot.addEventListener('click', () => {
-            setSlide(i);
-        });
+      dot.classList.toggle('active', i === index);
     });
+  }
 
-    updateCarousel(); // Initialize carousel position and dots 
+  function nextSlide() {
+    index = (index < items.length - 1) ? index + 1 : 0;
+    updateCarousel();
+  }
+
+  function setSlide(newIndex) {
+    index = newIndex;
+    updateCarousel();
+  }
+
+  // Auto-slide every `slideInterval` milliseconds
+  setInterval(nextSlide, slideInterval);
+
+  // Set up dot click events
+  dots.forEach((dot, i) => {
+    dot.addEventListener('click', () => {
+      setSlide(i);
+    });
   });
 
+  updateCarousel(); // Initialize carousel position and dots 
+});
 
-  document.getElementById("contactForm").addEventListener("submit", function(e) {
+
+document.getElementById("contactForm").addEventListener("submit", function (e) {
   e.preventDefault();
 
   const form = e.target;
@@ -125,16 +125,16 @@ function searchServices() {
     method: "POST",
     body: formData,
   })
-  .then(response => response.json())
-  .then(data => {
-    alert("Pesan berhasil dikirim!");
-    form.reset();
-  })
-  .catch(error => {
-    console.error(error);
-    alert("Gagal mengirim pesan.");
-  });
+    .then(response => response.json())
+    .then(data => {
+      alert("Pesan berhasil dikirim!");
+      form.reset();
+    })
+    .catch(error => {
+      console.error(error);
+      alert("Gagal mengirim pesan.");
+    });
 });
 
 
-  
+
